288578cb3d4cf5d820d3b53f4cd901ca86303ffc
asaid <a.said@solutionplus.net>
2025-01-10 17:13:04 +0200

    feat: add type annotations with variables and functions

---
+++ src/TypeAnnotations/index.ts
@@ -0,0 +1,25 @@
+
+
+//================ Type Annotations Variables ================
+
+let firstName: string = 'Ahmed'
+let lastName: string = 'Said'
+let age: number = 29
+let data: any;
+
+let all: (string | number | boolean) = 'said'
+
+let allArray: (string | number | boolean | (string | number | boolean)[])[] = ['ahmed', 'said', 'shehata', false, 2, ['1', 2, false]]
+
+
+//================ Type Annotations Functions ================
+
+function add(num1: number, num2: number): number {
+    return num1 + num2
+}
+
+add(1, 2)
+
+
+// =======================================================
+
